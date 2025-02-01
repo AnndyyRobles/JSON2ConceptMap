f22f6ef24fba338d4c036062645a447f1a5dfbd6
Andres <133448459+AnndyyRobles@users.noreply.github.com>
2025-02-01 17:34:05 -0600

    Update colors.ts

--- utils/colors.ts
+++ utils/colors.ts
@@ -1,10 +1,10 @@
 export const conceptMapColors = [
-  { bg: "bg-red-50", border: "border-red-500" },
-  { bg: "bg-orange-50", border: "border-orange-500" },
-  { bg: "bg-yellow-50", border: "border-yellow-400" },
-  { bg: "bg-green-50", border: "border-green-500" },
-  { bg: "bg-blue-50", border: "border-blue-500" },
-  { bg: "bg-pink-50", border: "border-pink-500" },
+  { bg: "bg-custom-red", border: "border-red-500" },
+  { bg: "bg-custom-orange", border: "border-orange-500" },
+  { bg: "bg-custom-yellow", border: "border-yellow-400" },
+  { bg: "bg-custom-green", border: "border-green-500" },
+  { bg: "bg-custom-blue", border: "border-blue-500" },
+  { bg: "bg-custom-pink", border: "border-pink-500" },
 ]
 
 export const getColorByIndex = (index: number) => {
